12ea6dd923d4a234e170293c7c8aa7f30751bd4c
itsregalo <itsregalo047@gmail.com>
2023-09-11 22:13:22 +0300

    added handling form logic

--- client/assets/js/authentication.js
+++ client/assets/js/authentication.js
@@ -32,3 +32,106 @@ const handleLoginForm = (e) => {
 }
 
 
+// validate login form
+const loginForm = document.querySelector('.login-form-area form');
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const loginEmail = document.querySelector('#login_email').value;
+    const loginPassword = document.querySelector('#login_password').value;
+
+    if (loginEmail === '' || loginPassword === '') {
+        setTimeout(() => {
+            document.querySelector('.message').innerHTML = `
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>Oops!</strong> All fields are required.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            `;
+        }, 1000);
+    } else {
+        setTimeout(() => {
+            document.querySelector('.message').innerHTML = `
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>Success!</strong> Login successful.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            `;
+        }, 1000);
+    }
+
+    handleLoginForm(e);
+});
+
+// register
+const submitRegisterForm = async (user) => {
+    try {
+        const response = await axios.post(`${base_url}/auth/register`, user);
+        const { data } = response;
+        window.location.href = "/client/auth/login.html";
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const handleRegisterForm = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const email = formData.get('email');
+    const username = formData.get('username');
+    const fullname = formData.get('fullname');
+    const password = formData.get('password');
+    const repeat_password = formData.get('repeat_password');
+
+    const user = {
+        email,
+        username,
+        fullname,
+        password,
+        repeat_password
+    }
+
+    submitRegisterForm(user);
+}
+
+// validate register form
+const registerForm = document.querySelector('#registration_form');
+
+registerForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const email = document.querySelector('#email').value;
+    const username = document.querySelector('#username').value;
+    const fullname = document.querySelector('#fullname').value;
+    const password = document.querySelector('#password').value;
+    const repeat_password = document.querySelector('#repeat_password').value;
+
+    if (email === '' || username === '' || fullname === '' || password === '' || repeat_password === '') {
+        setTimeout(() => {
+            document.querySelector('.message').innerHTML = `
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>Oops!</strong> All fields are required.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            `;
+        }, 1000);
+    } else {
+        setTimeout(() => {
+            document.querySelector('.message').innerHTML = `
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>Success!</strong> Registration successful.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            `;
+        }, 1000);
+    }
+
+    handleRegisterForm(e);
+});
+
+
+
+
+
